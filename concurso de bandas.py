3b94692dc649e6b76cbbcee35a75e957a8add363
Melqui Garcia <monterrosomelqui39@gmail.com>
2025-09-05 08:39:43 -0600

    creacion de programa

--- concurso de bandas.py
+++ concurso de bandas.py
@@ -1,1 +1,30 @@
-class
+class participante:
+    def __init__(self,nombre,institucion):
+        self.nombre = nombre
+        self.institucion = institucion
+
+    def mostrar_info(self):
+        return f"Nombre: {self.nombre}-Insitucion: {self.institucion}"
+
+class Bandasescolares(participante):
+    def __init__(self,nombre,institucion,categoria,puntaje):
+        super().__init__(nombre,institucion)
+        self._categoria = categoria
+        self._puntaje = puntaje
+        self.registrar_puntaje = {}
+
+    @property
+    def categoria(self):
+        return self._categoria
+
+    @categoria.setter
+    def categoria(self,categoria):
+        self._categoria = categoria
+
+    def mostrar_info(self):
+        if self._puntaje:
+            return f"Nombre: {self.nombre}-Insitucion: {self.institucion} - Categoría: {self._categoria}-Puntaje total: {self._puntaje}"
+        else:
+            return f"Nombre: {self.nombre}-Insitucion: {self.institucion} - Categoría: {self._categoria}"
+
+
