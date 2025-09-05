83b1835bdffb6fc1b77b16837411609445c15200
Melqui Garcia <monterrosomelqui39@gmail.com>
2025-09-05 09:43:17 -0600

    codigo corregido, lista de bandas

--- concurso de bandas.py
+++ concurso de bandas.py
@@ -6,7 +6,7 @@ class participante:
     def mostrar_info(self):
         return f"Nombre: {self.nombre}-Insitucion: {self.institucion}"
 
-class Bandasescolares(participante):
+class BandasEscolar(participante):
     def __init__(self,nombre,institucion,categoria,puntaje):
         super().__init__(nombre,institucion)
         self._categoria = categoria
@@ -21,18 +21,66 @@ class Bandasescolares(participante):
     def categoria(self,categoria):
         self._categoria = categoria
 
+    def registrar_puntajes(self):
+        print("Califique a la banda, segun los criterios del 1 al 10")
+
+        ritmo=int(input("Ritmo: "))
+        uniformidad=int(input("Uniformidad: "))
+        coreo=int(input("Coreografía: "))
+        alineacion=int(input("Alineación: "))
+        puntualidad=int(input("Puntualidad: "))
+
+        self._puntaje[self.nombre]={
+            "ritmo": ritmo,
+            "uniformidad": uniformidad,
+            "coreo": coreo,
+            "alineacion": alineacion,
+            "puntualidad": puntualidad,
+        }
+
+        suma_puntajes=ritmo+uniformidad+coreo+alineacion+puntualidad
+        self._puntaje=suma_puntajes
+
+
     def mostrar_info(self):
         if self._puntaje:
-            return f"Nombre: {self.nombre}-Insitucion: {self.institucion} - Categoría: {self._categoria}-Puntaje total: {self._puntaje}"
+            return f"Nombre: {self.nombre}-Insitucion: {self.institucion} - Categoría: {self._categoria} - Puntaje total: {self._puntaje} - Puntaje promedio: {self._puntaje/5}"
         else:
             return f"Nombre: {self.nombre}-Insitucion: {self.institucion} - Categoría: {self._categoria}"
 
-
 class concurso:
-    def __init__(self,nombre,fecha):
-        self.nombre = nombre
-        self.fecha = fecha
+    def __init__(self):
         self.bandas = {}
 
-    def InscribirBandas(self):
+    def InscribirBandas(self,banda):
+        if banda.nombre in self.bandas:
+            raise ValueError(f"La banda {banda.nombre} ya existe.")
+        self.bandas[banda.nombre] = banda
+
+    def registrar_Evaluacion(self,nombre_banda,puntajes):
+        if nombre_banda not in self.bandas:
+            raise ValueError(f"La banda {nombre_banda} no esta inscrita.")
+        self.bandas[nombre_banda] = puntajes
+
+    def registrar_evaluacion(self, nombre_banda, puntajes):
+        if nombre_banda not in self.bandas:
+            raise ValueError(f"La banda {nombre_banda} no está inscrita.")
+        banda = self.bandas[nombre_banda]
+        for i in puntajes:
+            banda.agregar_puntaje(i)
+
+    def listar_bandas(self):
+        print("-- Listado de bandas --")
+        for banda in self.bandas.values():
+            print(banda.mostrar_info())
+
+
+
+
+
+
+
+
+
+
 
